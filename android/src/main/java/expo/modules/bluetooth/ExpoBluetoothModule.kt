5c266b8dc0604343901f2dd6209da08c8c6fa142
Christoffer Carlsson <cc@christofferc.com>
2024-06-22 14:26:04 +0200

    Add placeholder implementations for iOS and Android

--- android/src/main/java/expo/modules/bluetooth/ExpoBluetoothModule.kt
+++ android/src/main/java/expo/modules/bluetooth/ExpoBluetoothModule.kt
@@ -3,45 +3,152 @@ package expo.modules.bluetooth
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
-class ExpoBluetoothModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoBluetooth')` in JavaScript.
-    Name("ExpoBluetooth")
+private const val ERROR_NOT_IMPLEMENTED = 0
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
+data class Device(val uuid: String, val name: String, val rssi: Int)
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+class DeviceManager {
+    var onConnect: ((String, String, Int) -> Unit)? = null
+    var onDisconnect: ((String) -> Unit)? = null
+    var onChange: ((String, String, ByteArray) -> Unit)? = null
+    var onWrite: ((String, String, ByteArray) -> Unit)? = null
+    var onError: ((Byte, String?, String?) -> Unit)? = null
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
+    private fun deviceError(code: Int, reason: String, device: Device?) {
+        onError?.invoke(code.toByte(), reason, device?.uuid ?: "")
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
+    private fun managerError(code: Int, reason: String) {
+        deviceError(code, reason, null)
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ExpoBluetoothView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExpoBluetoothView, prop: String ->
-        println(prop)
-      }
+    private fun notImplemented(fn: String) {
+        managerError(ERROR_NOT_IMPLEMENTED, "$fn is not implemented yet")
     }
-  }
+
+    fun startAdvertising(name: String, servicesJSON: String) {
+        notImplemented("startAdvertising")
+    }
+
+    fun stopAdvertising() {
+        notImplemented("stopAdvertising")
+    }
+
+    fun startScanning(services: List<String>, reconnect: Boolean) {
+        notImplemented("startScanning")
+    }
+
+    fun stopScanning() {
+        notImplemented("stopScanning")
+    }
+
+    fun connect(device: String, reconnect: Boolean) {
+        notImplemented("connect")
+    }
+
+    fun disconnect(device: String) {
+        notImplemented("disconnect")
+    }
+
+    fun read(device: String, characteristic: String) {
+        notImplemented("read")
+    }
+
+    fun subscribe(device: String, characteristic: String) {
+        notImplemented("subscribe")
+    }
+
+    fun unsubscribe(device: String, characteristic: String) {
+        notImplemented("unsubscribe")
+    }
+
+    fun write(device: String, characteristic: String, value: ByteArray, withResponse: Boolean) {
+        notImplemented("write")
+    }
+
+    fun set(device: String, characteristic: String, value: ByteArray) {
+        notImplemented("set")
+    }
+
+    fun notify(device: String, characteristic: String, value: ByteArray) {
+        notImplemented("notify")
+    }
+}
+
+class BluetoothModule : Module() {
+    private val deviceManager: DeviceManager = DeviceManager()
+
+    override fun definition() = ModuleDefinition {
+        Name("ExpoBluetooth")
+
+        Events("onConnect", "onDisconnect", "onChange", "onWrite", "onError")
+
+        OnCreate {
+            deviceManager.apply {
+                onConnect = { device, name, rssi ->
+                    sendEvent("onConnect", mapOf("device" to device, "name" to name, "rssi" to rssi))
+                }
+                onDisconnect = { device ->
+                    sendEvent("onDisconnect", mapOf("device" to device))
+                }
+                onChange = { device, characteristic, value ->
+                    sendEvent("onChange", mapOf("device" to device, "characteristic" to characteristic, "value" to value))
+                }
+                onWrite = { device, characteristic, value ->
+                    sendEvent("onWrite", mapOf("device" to device, "characteristic" to characteristic, "value" to value))
+                }
+                onError = { code, reason, device ->
+                    sendEvent("onError", mapOf("code" to code, "reason" to (reason ?: ""), "device" to (device ?: "")))
+                }
+            }
+        }
+
+        Function("startAdvertising") { name: String, servicesJSON: String ->
+            deviceManager.startAdvertising(name, servicesJSON)
+        }
+
+        Function("stopAdvertising") {
+            deviceManager.stopAdvertising()
+        }
+
+        Function("startScanning") { services: List<String>, reconnect: Boolean ->
+            deviceManager.startScanning(services, reconnect)
+        }
+
+        Function("stopScanning") {
+            deviceManager.stopScanning()
+        }
+
+        Function("connect") { device: String, reconnect: Boolean ->
+            deviceManager.connect(device, reconnect)
+        }
+
+        Function("disconnect") { device: String ->
+            deviceManager.disconnect(device)
+        }
+
+        Function("read") { device: String, characteristic: String ->
+            deviceManager.read(device, characteristic)
+        }
+
+        Function("subscribe") { device: String, characteristic: String ->
+            deviceManager.subscribe(device, characteristic)
+        }
+
+        Function("unsubscribe") { device: String, characteristic: String ->
+            deviceManager.unsubscribe(device, characteristic)
+        }
+
+        Function("write") { device: String, characteristic: String, value: ByteArray, withResponse: Boolean ->
+            deviceManager.write(device, characteristic, value, withResponse)
+        }
+
+        Function("set") { device: String, characteristic: String, value: ByteArray ->
+            deviceManager.set(device, characteristic, value)
+        }
+
+        Function("notify") { device: String, characteristic: String, value: ByteArray ->
+            deviceManager.notify(device, characteristic, value)
+        }
+    }
 }
--- ios/ExpoBluetoothModule.swift
+++ ios/ExpoBluetoothModule.swift
@@ -1,44 +1,172 @@
 import ExpoModulesCore
 
-public class ExpoBluetoothModule: Module {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  public func definition() -> ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoBluetooth')` in JavaScript.
-    Name("ExpoBluetooth")
+let ERROR_NOT_IMPLEMENTED = 0
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
+struct Device {
+    let uuid: String
+    let name: String
+    let rssi: Int8
+}
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+class DeviceManager {
+    var onConnect: ((String, String, Int8) -> Void)?
+    var onDisconnect: ((String) -> Void)?
+    var onChange: ((String, String, Data) -> Void)?
+    var onWrite: ((String, String, Data) -> Void)?
+    var onError: ((UInt8, String?, String?) -> Void)?
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
+    private func deviceError(_ code: Int, _ reason: String, _ device: Device?) {
+        onError?(UInt8(code), reason, device ? device.id : "")
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
+    private func managerError(_ code: Int, _ reason: String) {
+        deviceError(code, reason, nil)
     }
 
-    // Enables the module to be used as a native view. Definition components that are accepted as part of the
-    // view definition: Prop, Events.
-    View(ExpoBluetoothView.self) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { (view: ExpoBluetoothView, prop: String) in
-        print(prop)
-      }
+    private func notImplemented(_ fn: String) {
+        managerError(ERROR_NOT_IMPLEMENTED, "\(fn) is not implemented yet")
     }
-  }
+
+    func startAdvertising(_: String, _: String) {
+        notImplemented("startAdvertising")
+    }
+
+    func stopAdvertising() {
+        notImplemented("stopAdvertising")
+    }
+
+    func startScanning(_: [String], _: Bool) {
+        notImplemented("startScanning")
+    }
+
+    func stopScanning() {
+        notImplemented("stopScanning")
+    }
+
+    func connect(_: String, _: Bool) {
+        notImplemented("connect")
+    }
+
+    func disconnect(_: String) {
+        notImplemented("disconnect")
+    }
+
+    func read(_: String, _: String) {
+        notImplemented("read")
+    }
+
+    func subscribe(_: String, _: String) {
+        notImplemented("subscribe")
+    }
+
+    func unsubscribe(_: String, _: String) {
+        notImplemented("unsubscribe")
+    }
+
+    func write(_: String, _: String, _: Data, _: Bool) {
+        notImplemented("write")
+    }
+
+    func set(_: String, _: String, _: Data) {
+        notImplemented("set")
+    }
+
+    func notify(_: String, _: String, _: Data) {
+        notImplemented("notify")
+    }
+}
+
+public class BluetoothModule: Module {
+    lazy var deviceManager: DeviceManager = .init()
+
+    public func definition() -> ModuleDefinition {
+        Name("ExpoBluetooth")
+
+        Events("onConnect", "onDisconnect", "onChange", "onWrite", "onError")
+
+        OnCreate {
+            _ = self.deviceManager
+            self.deviceManager.onConnect = { (device: String, name: String, rssi: Int8) in
+                self.sendEvent("onConnect", [
+                    "device": device,
+                    "name": name,
+                    "rssi": rssi,
+                ])
+            }
+            self.deviceManager.onDisconnect = { (device: String) in
+                self.sendEvent("onDisconnect", [
+                    "device": device,
+                ])
+            }
+            self.deviceManager.onChange = { (device: String, characteristic: String, value: Data) in
+                self.sendEvent("onChange", [
+                    "device": device,
+                    "characteristic": characteristic,
+                    "value": value,
+                ])
+            }
+            self.deviceManager.onWrite = { (device: String, characteristic: String, value: Data) in
+                self.sendEvent("onWrite", [
+                    "device": device,
+                    "characteristic": characteristic,
+                    "value": value,
+                ])
+            }
+            self.deviceManager.onError = { (code: UInt8, reason: String?, device: String?) in
+                self.sendEvent("onError", [
+                    "code": code,
+                    "reason": reason ?? "",
+                    "device": device ?? "",
+                ])
+            }
+        }
+
+        Function("startAdvertising") { (name: String, servicesJSON: String) in
+            self.deviceManager.startAdvertising(name, servicesJSON)
+        }
+
+        Function("stopAdvertising") {
+            self.deviceManager.stopAdvertising()
+        }
+
+        Function("startScanning") { (services: [String], reconnect: Bool) in
+            self.deviceManager.startScanning(services, reconnect)
+        }
+
+        Function("stopScanning") {
+            self.deviceManager.stopScanning()
+        }
+
+        Function("connect") { (device: String, reconnect: Bool) in
+            self.deviceManager.connect(device, reconnect)
+        }
+
+        Function("disconnect") { (device: String) in
+            self.deviceManager.disconnect(device)
+        }
+
+        Function("read") { (device: String, characteristic: String) in
+            self.deviceManager.read(device, characteristic)
+        }
+
+        Function("subscribe") { (device: String, characteristic: String) in
+            self.deviceManager.subscribe(device, characteristic)
+        }
+
+        Function("unsubscribe") { (device: String, characteristic: String) in
+            self.deviceManager.unsubscribe(device, characteristic)
+        }
+
+        Function("write") { (device: String, characteristic: String, value: Data, withResponse: Bool) in
+            self.deviceManager.write(device, characteristic, value, withResponse)
+        }
+
+        Function("set") { (device: String, characteristic: String, value: Data) in
+            self.deviceManager.set(device, characteristic, value)
+        }
+
+        Function("notify") { (device: String, characteristic: String, value: Data) in
+            self.deviceManager.notify(device, characteristic, value)
+        }
+    }
 }
